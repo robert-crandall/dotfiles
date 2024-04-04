74d38352a3454cd50d289007f9f5639f2cc4d4ae
Robert Crandall <robert.crandall@gmail.com>
2024-04-04 15:48:54 -0700

    Silent starship

--- chezmoi/.chezmoiscripts/run_once_install-starship.sh
+++ chezmoi/.chezmoiscripts/run_once_install-starship.sh
@@ -2,5 +2,5 @@
 if  command -v starship >/dev/null 2>&1; then
     echo "starship already installed"
 else
-    curl -fsSL https://starship.rs/install.sh | sudo sh
+    curl -sL https://starship.rs/install.sh | sh -s -- -y
 fi
