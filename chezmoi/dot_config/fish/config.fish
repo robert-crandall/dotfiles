4e982f9a20dc870cc039fcd15a1f3626f2f68cde
Robert Crandall <robert.crandall@gmail.com>
2024-04-08 20:34:31 -0700

    Remove extra stuff :shrug:

--- chezmoi/dot_config/fish/config.fish
+++ chezmoi/dot_config/fish/config.fish
@@ -11,9 +11,6 @@ end
 
 if test -f "/opt/homebrew/bin/brew"
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    set -Ux fish_user_paths $fish_user_paths /usr/local/opt/mysql-client/bin
-    # fish_add_path /opt/homebrew/opt/libpq/bin # Unsure if I need this
-    set -Ux fish_user_paths $fish_user_paths /Users/robert-crandall/.asdf/shims
 end
 
 # Hide greeting
@@ -21,11 +18,3 @@ set fish_greeting
 
 # Init Starship
 starship init fish | source
-
-
-# ASDF 
-# Configuration for asdf should always be the last line in the ~/.zshrc file, after setting any $PATH configuration. - https://mac.install.guide/ruby/5
-# Trying fisher install rstacruz/fish-asdf instead
-# if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
-#     source /opt/homebrew/opt/asdf/libexec/asdf.fish
-# end
