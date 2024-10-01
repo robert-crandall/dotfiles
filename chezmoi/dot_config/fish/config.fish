ad3694301fa4bb21336aeb70c735fdf7f4518284
Robert Crandall <robert.crandall@gmail.com>
2024-10-01 16:52:52 -0700

    Add asdf

--- chezmoi/dot_config/fish/config.fish
+++ chezmoi/dot_config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+if command -v brew >/dev/null 2>&1
+    set -l asdf_path (brew --prefix asdf)/libexec/asdf.fish
+    if test -f $asdf_path
+        source $asdf_path
+    end
+end
