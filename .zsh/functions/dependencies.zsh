720e2db99219cbd4d838d8bd27c4ac2118789950
Robert Crandall <robert-crandall@github.com>
2025-03-11 11:24:21 -0700

    Rename dependencies

--- .zsh/functions/dependencies.zsh
+++ .zsh/functions/dependencies.zsh
@@ -5,3 +5,8 @@ ASDF_PATH="/opt/homebrew/opt/asdf/libexec/asdf.sh"
 if [[ -f "$ASDF_PATH" ]]; then
     source "$ASDF_PATH"
 fi
+
+LIBPQ_PATH="/opt/homebrew/opt/libpq/bin"
+if [[ -d "$LIBPQ_PATH" ]]; then
+    export PATH="$LIBPQ_PATH:$PATH"
+fi
